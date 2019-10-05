f4b48bb6d8611ac0ac871e6ea79bd746f337da8a
Juan Villegas <jvillegas@jidadesarrollos.com>
2019-10-05 14:53:43 -0430

    first commit

---
+++ config/database.php
@@ -0,0 +1,26 @@
+<?php
+
+	class dataBase{
+		
+		private $host = "localhost";
+		private $db = "phplearning";
+		private $user = "root";
+		private $pass = "123456";
+		private $conn;
+		
+		public function getConnection(){
+			
+			$this->conn = null;
+			
+			try{
+				$this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db, $this->user, $this->pass);
+				$this->conn->exec("set names utf8");
+			}catch(PDOException $e){
+				echo "Connection error: " . $e->getMessage();
+			}
+			return $this->conn;			
+			
+		}
+		 
+	}
+?>
