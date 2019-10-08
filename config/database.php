777442baa69c6116510be56d056258059a21c4fe
Juan Villegas <jvillegas@jidadesarrollos.com>
2019-10-08 07:50:32 -0430

    second commit

--- config/database.php
+++ config/database.php
@@ -1,26 +1,28 @@
 <?php
 
-	class dataBase{
-		
-		private $host = "localhost";
-		private $db = "phplearning";
-		private $user = "root";
-		private $pass = "123456";
-		private $conn;
-		
-		public function getConnection(){
-			
-			$this->conn = null;
-			
-			try{
-				$this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db, $this->user, $this->pass);
-				$this->conn->exec("set names utf8");
-			}catch(PDOException $e){
-				echo "Connection error: " . $e->getMessage();
-			}
-			return $this->conn;			
-			
-		}
-		 
-	}
+class dataBase {
+
+    private $host = "localhost";
+    private $db = "phplearning";
+    private $user = "root";
+    private $pass = "123456";
+    private $conn;
+
+    public function getConnection() {
+
+        $this->conn = null;
+
+        try {
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db, $this->user, $this->pass);
+            $this->conn->exec("set names utf8");
+        }
+        catch (PDOException $e) {
+            echo "Connection error: " . $e->getMessage();
+        }
+        return $this->conn;
+
+    }
+
+}
+
 ?>
--- models/product.php
+++ models/product.php
@@ -1,37 +1,37 @@
 <?php
 
-	class Product{
-		
-		private $conn;
-		private $table = "products";
+class Product {
 
-		public $id;
-		public $name;
-		public $description;
-		public $price;
-		public $category_id;
-		public $category_name;
-		public $created;
-		
-		public function __construct($db){
-			$this->conn = $db;
-		}
-		
-		public function read(){
-			
-			$query = "SELECT c.name as category_name, p.id, p.name, p.description, p.price, p.category_id, p.created
+    private $conn;
+    private $table = "products";
+
+    public $id;
+    public $name;
+    public $description;
+    public $price;
+    public $category_id;
+    public $category_name;
+    public $created;
+
+    public function __construct($db) {
+        $this->conn = $db;
+    }
+
+    public function read() {
+
+        $query = "SELECT c.name as category_name, p.id, p.name, p.description, p.price, p.category_id, p.created
 				FROM
                 " . $this->table . " p 
 				LEFT JOIN categories c ON p.category_id = c.id
 				ORDER BY p.created DESC";
-			
-			$stmt = $this->conn->prepare($query);
-			$stmt->execute();	
-			
-			return $stmt;
-			
-		}
-		
-	}
+
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+
+        return $stmt;
+
+    }
+
+}
 
 ?>
